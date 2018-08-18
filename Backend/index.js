4876788af3619a586eb8f43cc372f3a36547211c
Petteri <Buggis123buggis@gmail.com>
2018-08-18 11:01:24 +0200

    push test

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
- var originalMenu = require('./cafeMenu.js')
+var originalMenu = require('./cafeMenu.js')
 var db;
 
 MongoClient.connect('mongodb://localhost:27017', function (error, client) {
@@ -9,23 +9,24 @@ MongoClient.connect('mongodb://localhost:27017', function (error, client) {
     console.log(error);
   } else {
     console.log('Successfully connect to the database!');
-    db = client.db('test');
-
-    db.collection('CaMenu').insert(originalMenu);
+    db = client.db('Cafe');
+// behöver en check om den finns.
+// db.collection('CaMenu').insert(originalMenu);
   }
 });
 
 var app = express();
 
 app.get('/', function (request, response) {
-  db.collection('cities').find().toArray(function (error, result) {
-    if (error) {
-      response.status(500).send({});
-      return;
-    }
-  console.log(result);
-
-  });
+  // test push
+  // db.collection('cities').find().toArray(function (error, result) {
+  //   if (error) {
+  //     response.status(500).send({});
+  //     return;
+  //   }
+  // console.log(result);
+  //
+  // });
 });
 
 app.listen(3000, function () {

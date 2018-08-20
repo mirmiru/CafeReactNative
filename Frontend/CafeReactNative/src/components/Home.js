39367c35de99524ff3d3d1a6c129e60e6247c8ea
Milja V <mmcvirtanen@gmail.com>
2018-08-20 11:24:01 +0200

    testing merge

--- Frontend/CafeReactNative/src/components/Home.js
+++ Frontend/CafeReactNative/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image, KeyboardAvoidingView, Button } from 'react-native';
 import InnerMargin from './innerMargin';
+import Menu from './Menu.js';
 
 export default class Home extends Component {
   static navigationOptions = {
@@ -9,8 +10,8 @@ export default class Home extends Component {
 
   render() {
     return (
-
       <View style={styles.container}>
+        <Menu></Menu>
       <Button title="Show me more of the app" onPress={this._showMoreApp} />
           <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
       </View>
